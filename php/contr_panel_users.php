b4a8ce04658828b2db162a059780796716d6ac16
iab-60 <240783@edu.p.lodz.pl>
2023-12-15 11:39:11 +0100

    Added the ability to block users

--- php/contr_panel_users.php
+++ php/contr_panel_users.php
@@ -2,6 +2,27 @@
 
 declare(strict_types=1);
 
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["block_user"])) {
+
+    $user_id = (int)$_POST["user_id"];
+
+    try {
+
+        require_once 'connection.php';
+        require_once "contr_panel_users_model.php";
+
+        delete_user($pdo, $user_id);
+
+        $pdo = null;
+
+        header("Location: ../views/contr_panel_users_view.php");
+        die();
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+}
+
 function get_users(): void
 {
     require_once "connection.php";
@@ -10,13 +31,14 @@ function get_users(): void
     $stmt = fetch_users($pdo);
     if ($stmt){
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            $id = $row['user_id'];
+            $id = (int)$row['user_id'];
             $name = ucfirst($row['name']);
             $lastname = ucfirst($row['last_name']);
             $email = $row['email'];
 
-            echo "<div class='user-wrap'>";
+            echo "<form class='user-wrap' action='../php/contr_panel_users.php' method='post'>";
                 echo "<label class='user-content' style='width: 10%'>$id</label>";
+                echo "<input type='hidden' name='user_id' value='$id'>";
                 echo "<label class='user-content' style='width: 20%'>$name</label>";
                 echo "<label class='user-content' style='width: 20%'>$lastname</label>";
                 echo "<label class='user-content' style='width: 20%'>$email</label>";
@@ -24,9 +46,9 @@ function get_users(): void
                     echo "<button>View Bookcase</button>";
                 echo "</a>";
                 echo "<div class='user-content' style='width: 15%'>";
-                echo "<button>Delete</button>";
+                echo "<button type='submit' name='block_user'>Block</button>";
                 echo "</div>";
-            echo "</div>";
+            echo "</form>";
         }
     }
 }
--- php/contr_panel_users_model.php
+++ php/contr_panel_users_model.php
@@ -1,9 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 function fetch_users(object $pdo)
 {
     $query = "SELECT * FROM users";
     $stmt = $pdo->prepare($query);
     $stmt->execute();
     return $stmt;
+}
+
+function delete_user(object $pdo, int $user_id): void
+{
+    $query = "UPDATE users SET role = 'blocked' WHERE user_id = :user_id";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":user_id", $user_id);
+    $stmt->execute();
 }
